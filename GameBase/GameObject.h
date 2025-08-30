d62d68599ab3ac1a707f665fd84e0571a5eac1ad
Tkm1Hub <takumis_17214@icloud.com>
2025-08-30 15:44:36 +0900

    8/30
    ・オブジェクトのインターフェース追加

---
+++ GameBase/GameObject.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "DxLib.h"
+#include <iostream>
+class IGameObject
+{
+public:
+	virtual ~IGameObject() = default;
+	virtual void Init() abstract;
+	virtual void Load() abstract;
+	virtual void Update() abstract;
+	virtual void Draw() abstract;
+
+	VECTOR GetPosition() const { return pos; }
+	std::string GetName() const { return name; }
+
+protected:
+	std::string name = "null";
+	VECTOR pos = VGet(0.0f, 0.0f, 0.0f);
+	int modelHandle = -1;
+};
